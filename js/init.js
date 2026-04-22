4693d7c98a2b158347310965eda41cfa73b5330c
michael-ngai <michaelngai.hf@gmail.com>
2026-04-22 22:18:37 +1000

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,28 @@
+// ── INIT ──
+updHdr();
+setSyncStatus('loading');
+renderBudget();
+document.getElementById('habit-date-hidden').value=logicalToday;
+updateDateDisplay();
+document.getElementById('tt-date-hidden').value=logicalToday;
+updateTTDateDisplay();
+var timeLabels=['12:00am','12:15am','12:30am','12:45am'];
+for(var h=1;h<=11;h++){['00','15','30','45'].forEach(function(m){timeLabels.push(h+':'+m+'am');});}
+timeLabels.push('12:00pm','12:15pm','12:30pm','12:45pm');
+for(var h=1;h<=11;h++){['00','15','30','45'].forEach(function(m){timeLabels.push(h+':'+m+'pm');});}
+timeLabels.push('11:00pm','11:15pm','11:30pm','11:45pm');
+['ce-start','ce-end'].forEach(function(id){
+  var sel=document.getElementById(id);
+  if(!sel)return;
+  timeLabels.forEach(function(t){var o=document.createElement('option');o.value=t;o.textContent=t;sel.appendChild(o);});
+});
+document.getElementById('ce-date-hidden').value=todayStr;
+if(window._updateCEDate)window._updateCEDate();
+renderTodo();
+renderHistory();
+Promise.all([renderH(),renderSav(),renderEx()]).then(function(){
+  setSyncStatus('ok');
+}).catch(function(){
+  setSyncStatus('err');
+});
+setInterval(updHdr,1000);
